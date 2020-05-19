d6eed3b24bfd8a37e3f027ba0d3aaecd70f03ba3
Piotr Mitręga <piotr.mitrega@catenda.no>
2020-05-19 19:25:45 +0200

    Remove stuff accidentally left when trying to fix yarn install

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-#!/usr/bin/env node
-process.env.UV_THREADPOOL_SIZE = 128;
-
 import App from './App';
 
 ReactDOM.render(
